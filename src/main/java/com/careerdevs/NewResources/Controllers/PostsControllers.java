12403d44b24b660e12867ef8d0049cb1736cb3fb
“JazlynL” <lubenjazlyn35@gmail.com>
2022-04-02 17:57:45 -0400

    Completed Posts Controllers and Models class.

--- src/main/java/com/careerdevs/NewResources/Controllers/PostsControllers.java
+++ src/main/java/com/careerdevs/NewResources/Controllers/PostsControllers.java
@@ -1,10 +1,14 @@
 package com.careerdevs.NewResources.Controllers;
 
+import com.careerdevs.NewResources.Models.PostModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -19,17 +23,30 @@ public class PostsControllers {
 
     // getting posts by id using pathVariable
     @GetMapping("/{id}")
-    public ResponseEntity postsOfUsers(RestTemplate restTemplate,
+    public Object postsOfUsers(RestTemplate restTemplate,
                                        @PathVariable("id") int postId) {
         try {
             String url = "https://gorest.co.in/public/v2/posts/"+ postId ;
-//            String token = env.getProperty("API_KEY");
-//            url += "&access-token=" + token;
 
-            return new ResponseEntity<>(restTemplate.getForObject(url, Object.class),HttpStatus.OK);
+            String token = env.getProperty("API_KEY");
+             url += "?access-token=" + token;
 
 
-        } catch (Exception e) {
+
+          //refactored code ... Another way
+//            var user = restTemplate.getForObject(url, PostModel.class);
+//            assert user != null;
+
+            System.out.println(url);
+            return new ResponseEntity<>(restTemplate.getForObject(url, PostModel.class),HttpStatus.OK);
+
+            // new return statement with refactored code
+//            return user;
+
+        } catch (HttpClientErrorException.NotFound exception) {
+            //resource not found  error will output a 404 HTTP status code response
+            return new ResponseEntity("user was not found in system ", HttpStatus.NOT_FOUND);
+        }catch (Exception e) {
             System.out.println(e.getClass());
             return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 
@@ -61,6 +78,83 @@ public class PostsControllers {
             System.out.println(e.getMessage());
             return  new ResponseEntity<>(e.getClass(),HttpStatus.INTERNAL_SERVER_ERROR);
 
+        }
+    }
+
+    @PostMapping
+
+    public ResponseEntity <Object> postUser(RestTemplate restTemplate,
+                                              @RequestBody PostModel postsUser){
+
+        try{
+            String url = "https://gorest.co.in/public/v2/posts/";
+            String token = env.getProperty("API_KEY");
+            url+= "?access-token="+token;
+            System.out.println(url);
+
+
+            HttpEntity<PostModel> request = new HttpEntity<>(postsUser);
+
+            System.out.println("Successfully created new user: " + postsUser );
+
+            //just a note of an alternative way to write the code.
+            // PostModel createdPost = restTemplate.postForObject(url, request, PostModel.class);
+
+            ResponseEntity<PostModel> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.POST,
+                    request,
+                    PostModel.class
+            );
+
+            return new ResponseEntity<>(response.getBody(),HttpStatus.CREATED);
+
+
+
+
+        }catch (HttpClientErrorException.NotFound exception) {
+            //resource not found  error will output a 404 HTTP status code response
+            return new ResponseEntity("user was not found in system ", HttpStatus.NOT_FOUND);}
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            return new ResponseEntity(e.getClass(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping
+
+    public ResponseEntity<Object> finalPut(RestTemplate restTemplate,
+                                           @RequestBody PostModel putPost){
+
+        try{
+
+            //remember to use the get ID method in the Post Model class,
+            String url = "https://gorest.co.in/public/v2/posts/" +putPost.getId();
+            String token = env.getProperty("API_KEY");
+            url+= "?access-token="+token;
+
+            //setting up the put request
+            HttpEntity<PostModel> request = new HttpEntity<>(putPost);
+
+            System.out.println("Successfully updated user information:\n"+ putPost);
+            System.out.println(url);
+            // then we will be setting up the response
+            ResponseEntity<PostModel> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.PUT,
+                    request,
+                    PostModel.class
+            );
+
+            return new ResponseEntity<>(response.getBody(),HttpStatus.CREATED);
+
+
+        }catch (HttpClientErrorException.NotFound exception) {
+            //resource not found  error will output a 404 HTTP status code response
+            return new ResponseEntity("user was not found in system ", HttpStatus.NOT_FOUND);}
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            return new ResponseEntity(e.getClass(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
--- src/main/java/com/careerdevs/NewResources/Models/PostModel.java
+++ src/main/java/com/careerdevs/NewResources/Models/PostModel.java
@@ -1,4 +1,68 @@
 package com.careerdevs.NewResources.Models;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+/*"id": 1432,
+    "user_id": 2937,
+    "title": "Tergum ascit ea carbo distinctio asperiores vulgo compono velit ait vos concido pariatur accedo tabesco.",
+    "body": "Calco aut audax. Tibi debeo hic.
+     Denuncio adeptio pecus. Verumtamen eligendi pecco.
+     Certo voluptatem vae. Vulnero acer eum.
+     Sponte conduco thesaurus. Coadunatio aut deficio.
+     Sperno verus arbitro. Synagoga hic angustus.
+      Curso textor curtus. Theca doloremque sortitus. Vapulus congregatio vulgivagus. Alias qui via. Corpus suffragium velut. Adflicto eos vix. Bene aggredior dedico. Cicuta cubo vergo."
+*/
 public class PostModel {
+    private int id;
+
+/*The JsonProperty annotation is used
+to map property names with JSON keys during serialization and deserialization.
+By default, if you try to serialize a POJO,
+ the generated JSON will have keys mapped to the fields of the POJO.
+ */
+    @JsonProperty("user_id")
+    private int userId;
+    private String title;
+    private String body;
+
+    public PostModel(){
+
+
+    }
+
+    public PostModel(int id, int userId,String title,String body){
+        this.id = id;
+        this.userId = userId;
+        this.title = title;
+        this.body = body;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return "PostModel{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
 }
